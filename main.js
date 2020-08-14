f3567bc2b84f99e22b58905ec170c9e5b2d5333d
HuemanTarget <joshbasche@live.com>
2020-08-14 11:09:56 -0700

    add event listeners

--- main.js
+++ main.js
@@ -14,3 +14,66 @@ let xPos = document.getElementById("xPos");
 let yPos = document.getElementById("yPos");
 let itemColor = document.getElementById("itemColor");
 let radSlide = document.getElementById("radius");
+
+//variables
+let isDragging = false;
+let startX, startY;
+let selectedShapeIndex;
+
+//canvas mouse movements
+canvas.onmousedown = handleMouseDown;
+canvas.onmousemove = handleMouseMove;
+canvas.onmouseup = handleMouseUp;
+canvas.onmouseout = handleMouseOut;
+
+//shapes array to store shape objects
+let shapes = [];
+
+//Event Listener to add shape objects to the shapes array
+rect.addEventListener("click", function () {
+  shapes.push({
+    name: "rectangle",
+    x: Math.floor(Math.random() * 450),
+    y: Math.floor(Math.random() * 450),
+    width: 60,
+    height: 50,
+    color: "black",
+  });
+  drawAll();
+});
+
+circ.addEventListener("click", function () {
+  shapes.push({
+    name: "circle",
+    x: Math.floor(Math.random() * 450),
+    y: Math.floor(Math.random() * 450),
+    radius: 50,
+    color: itemColor.value,
+    border: null,
+  });
+  drawAll();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

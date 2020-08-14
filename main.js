31d512d95420fe30aa46887dee6ab5fafadc9cae
HuemanTarget <joshbasche@live.com>
2020-08-14 11:32:51 -0700

    add draw all function

--- main.js
+++ main.js
@@ -14,11 +14,31 @@ let xPos = document.getElementById("xPos");
 let yPos = document.getElementById("yPos");
 let itemColor = document.getElementById("itemColor");
 let radSlide = document.getElementById("radius");
+let hSlide = document.getElementById("height");
+let wSlide = document.getElementById("width");
 
 //variables
 let isDragging = false;
 let startX, startY;
 let selectedShapeIndex;
+
+//setup canvas bounding areas on resize
+function reOffset() {
+  let BB = canvas.getBoundingClientRect();
+  offsetX = BB.left;
+  offsetY = BB.top;
+}
+let offsetX, offsetY;
+reOffset();
+window.onscroll = function (e) {
+  reOffset();
+};
+window.onresize = function (e) {
+  reOffset();
+};
+canvas.onresize = function (e) {
+  reOffset();
+};
 
 //canvas mouse movements
 canvas.onmousedown = handleMouseDown;
@@ -53,6 +73,23 @@ circ.addEventListener("click", function () {
   });
   drawAll();
 });
+
+//draw shapes at their current position
+const drawAll = () => {
+  ctx.clearRect(0, 0, ch, cw);
+  for (let i = 0; i < shapes.length; i++) {
+    let shape = shapes[i];
+    if (shape.radius) {
+      ctx.beginPath();
+      ctx.arc(shape.x, shape.y, shape.radius, 0, Math.PI * 2);
+      ctx.fillStyle = shape.color;
+      ctx.fill();
+    } else if (shape.width) {
+      ctx.fillStyle = shape.color;
+      ctx.fillRect(shape.x, shape.y, shape.width, shape.height);
+    }
+  }
+};
 
 
 
